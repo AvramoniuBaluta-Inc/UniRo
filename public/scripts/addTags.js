d8ab5f2918f3769e676d451e761fce2f7b894da1
CezarBaluta <cezar.baluta@yahoo.com>
2022-01-12 23:25:37 +0200

    addArray bug fix

--- public/scripts/addTags.js
+++ public/scripts/addTags.js
@@ -3,8 +3,7 @@ const tags = [];
 module.exports = {
   addArray: function add_to_array(req, res) {
     var tag = "tag1";
-    console.log(req.body[tag]);
-    for (var i = "1"; 1; i++) {
+    for (var i = 1; 1; i++) {
       tag = "tag" + i;
       if (req.body[tag] === undefined) break;
       tags[i - "0" - 1] = req.body[tag];
