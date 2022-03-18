16ad7cfc1e6e64d8c31cbaa890bfe83d5c058e4b
CezarBaluta <cezar.baluta@yahoo.com>
2022-03-18 21:52:19 +0200

    filter functionality implementation

--- backend_scripts/uniFunctions.js
+++ backend_scripts/uniFunctions.js
@@ -7,4 +7,32 @@ module.exports = {
       if (uni === null) return i;
     }
   },
+  oras: function oras(uni, req) {
+    if (req === "Alege...") return true;
+    return uni.oras === req;
+  },
+  materii: function materii(uni, req) {
+    if (req === "Alege...") return true;
+    var cnt = uni.tags.length;
+    for (var i = 0; i < cnt; i++) {
+      if (uni.tags[i] === req) return true;
+    }
+    return false;
+  },
+  rating: function rating(uni, req) {
+    if (req === "Alege...") return true;
+    return true;
+  },
+  specializare: function specializare(uni, req) {
+    if (req === "Alege...") return true;
+    var cnt = uni.tags.length;
+    for (var i = 0; i < cnt; i++) {
+      if (uni.tags[i] === req) return true;
+    }
+    return false;
+  },
+  distanta: function distanta(uni, req) {
+    // if (req === "Alege...") return true;
+    return true;
+  },
 };
--- routers/universities.js
+++ routers/universities.js
@@ -14,6 +14,7 @@ router.get("/", function (req, res) {
     for (var i = 1; i < cnt; i++) {
       uniArray[i] = await University.findById(i).exec();
     }
+
     res.render("universities", {
       lungime: cnt,
       uniArray: uniArray,
@@ -23,7 +24,33 @@ router.get("/", function (req, res) {
 });
 
 router.post("/", function (req, res) {
-  res.send("About birds");
+  var cnt;
+  var cntFiltered = 1;
+  var uniArray = [];
+  var uniArrayFiltered = [];
+  (async () => {
+    cnt = await uniFunctions.number_of_elements();
+    for (var i = 1; i < cnt; i++) {
+      uniArray[i] = await University.findById(i).exec();
+    }
+    for (var i = 1; i < cnt; i++) {
+      if (
+        uniFunctions.oras(uniArray[i], req.body.oras) &&
+        uniFunctions.materii(uniArray[i], req.body.materii) &&
+        uniFunctions.rating(uniArray[i], req.body.rating) &&
+        uniFunctions.specializare(uniArray[i], req.body.specializare) &&
+        uniFunctions.distanta(uniArray[i], req.body.distanta)
+      ) {
+        uniArrayFiltered[cntFiltered] = uniArray[i];
+        cntFiltered++;
+      }
+    }
+    res.render("universities", {
+      lungime: cntFiltered,
+      uniArray: uniArrayFiltered,
+      materii: materii,
+    });
+  })();
 });
 
 module.exports = router;
