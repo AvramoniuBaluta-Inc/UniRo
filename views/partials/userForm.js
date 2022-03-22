daef98bac6d545edff759bc0056a3e3e55eec83b
calinstefan025 <clavramoniu@gmail.com>
2022-03-22 21:07:05 +0200

    contact 2.0

---
+++ views/partials/userForm.js
@@ -0,0 +1,53 @@
+<form class="row g-3 form1">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">
+      Nume
+    </label>
+    <input type="email" class="form-control" id="inputEmail4"></input>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">
+      Prenume
+    </label>
+    <input type="text" class="form-control" id="inputPassword4"></input>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">
+      Mesaj
+    </label>
+    <textarea
+      type="text"
+      class="form-control"
+      id="inputAddress"
+      placeholder="Scrie mesajul aici..."
+    ></textarea>
+  </div>
+
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">
+      City
+    </label>
+    <input type="text" class="form-control" id="inputCity"></input>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">
+      Oras
+    </label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">
+      Zip
+    </label>
+    <input type="text" class="form-control" id="inputZip"></input>
+  </div>
+
+  <div class="col-12">
+    <button type="submit" class="hero-btn submit-btn">
+      Trimite
+    </button>
+  </div>
+</form>;
