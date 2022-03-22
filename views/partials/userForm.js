f64cbe44dc49f60ba68241f899187df26d6fbdad
calinstefan025 <clavramoniu@gmail.com>
2022-03-22 22:09:22 +0200

    contact 3.0

--- views/partials/userForm.js
+++ views/partials/userForm.js
@@ -24,28 +24,13 @@
   </div>
 
   <div class="col-md-6">
-    <label for="inputCity" class="form-label">
-      City
-    </label>
-    <input type="text" class="form-control" id="inputCity"></input>
-  </div>
-  <div class="col-md-4">
     <label for="inputState" class="form-label">
-      Oras
+      Localitate
     </label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">
-      Zip
-    </label>
-    <input type="text" class="form-control" id="inputZip"></input>
+    <input type="text" class="form-control" id="inputState"></input>
   </div>
 
-  <div class="col-12">
+  <div class="col-md-6">
     <button type="submit" class="hero-btn submit-btn">
       Trimite
     </button>
