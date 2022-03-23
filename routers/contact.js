51b004498807a6738a326a7c50a20b5847131e40
calinstefan025 <clavramoniu@gmail.com>
2022-03-23 20:53:40 +0200

    contact 5.0

--- routers/contact.js
+++ routers/contact.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var orase = require("../public/scripts/orase");
+var materii = require("../public/scripts/materii.js");
+var specializari = require("../public/scripts/specializari.js");
 
 router.get("/", function (req, res) {
-  res.render("contact");
+  res.render("contact", {
+    specializari: specializari,
+    materii: materii,
+    orase: orase,
+  });
 });
 
 module.exports = router;
