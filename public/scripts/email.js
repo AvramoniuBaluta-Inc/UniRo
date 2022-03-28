354e26163dd5331aeed1c4fd687dede656756401
calinstefan025 <clavramoniu@gmail.com>
2022-03-28 11:35:03 +0300

    email implementation v1.0

--- public/scripts/email.js
+++ public/scripts/email.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".form1");
 
+function sendMsg(e) {
+  e.preventDefault();
+
+  const nume = document.getElementById("name"),
+    prenume = document.getElementById("prenume"),
+    loc = document.getElementById("loc"),
+    msg = document.getElementById("msg");
+
+  Email.send({
+    SecureToken: "61d10347-3d4f-4d6b-8dac-459786c83f60",
+    To: "uniro.eu@gmail.com",
+    From: "uniro.eu@gmail.com",
+    Subject: nume.value + " " + prenume.value + " - " + loc.value,
+    Body: msg.value,
+  }).then((message) => alert(message));
+}
+
+form.addEventListener("submit", sendMsg);
