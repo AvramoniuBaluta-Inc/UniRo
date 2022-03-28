068fd73475c6a96dabbc93f7898c6032fccf9d7b
calinstefan025 <clavramoniu@gmail.com>
2022-03-29 00:10:56 +0300

    Update displayCustomMap.js

--- public/scripts/displayCustomMap.js
+++ public/scripts/displayCustomMap.js
@@ -1,7 +1,11 @@
 function initMap() {
   const latitudine = document.getElementById("latitude");
   const longitudine = document.getElementById("longitude");
-  const point = { lat: 44.43861789060006, lng: 26.049495182208684 };
+
+  const lat1 = parseFloat(latitudine.value);
+  const long1 = parseFloat(longitudine.value);
+
+  const point = { lat: lat1, lng: long1 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
