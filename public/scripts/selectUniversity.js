4c012225bf81f0835beae3efe86192bf034749e1
CezarBaluta <cezar.baluta@yahoo.com>
2022-04-09 00:54:57 +0300

    editUniversity update

--- public/scripts/selectUniversity.js
+++ public/scripts/selectUniversity.js
@@ -1,16 +1,24 @@
+
+
 document.querySelectorAll('[universitate]').forEach(item => {
     item.addEventListener('click', event => {
+      console.log(document.getElementById("add_university").children[1][2]);
         document.getElementById("edit_university").style.display = "none";
         document.getElementById("add_university").style.display = "block";
-      //  document.getElementById("add_university").children[1].children[12].value = 0;
-    //    document.getElementById("add_university").children[1].children[0].value = 0;
-    //    document.getElementById("add_university").children[1].children[1].value = 0;
-      //  document.getElementById("add_university").children[1].children[2].value = 0;
-      //  document.getElementById("add_university").children[1].children[3].value = 0;
-       // document.getElementById("add_university").children[1].children[4].value = 0;
-      //  document.getElementById("add_university").children[1].children[5].value = 0;
-      //  document.getElementById("add_university").children[1].children[6].value = 0;
-      //  document.getElementById("add_university").children[1].children[7].value = 0;
+        document.getElementById("add_university").children[1][12].value = 0;
+        document.getElementById("add_university").children[1][0].value = item.children[0].children[0].children[1].children[0].children[0].children[0].innerText;
+        document.getElementById("add_university").children[1][1].value = item.children[0].children[0].children[1].children[0].children[3].innerText;
+        document.getElementById("add_university").children[1][2].value = item.children[1].children[0].innerText;
+        document.getElementById("add_university").children[1][3].value = item.children[1].children[0].innerText;
+        document.getElementById("add_university").children[1][4].value = item.children[1].children[1].innerText;
+        document.getElementById("add_university").children[1][5].value = item.children[1].children[2].innerText;
+        document.getElementById("add_university").children[1][6].value = item.children[1].children[3].innerText;
+        document.getElementById("add_university").children[1][7].value = item.children[1].children[4].innerText;
       document.getElementById("add_university").children[1][12].value = 0;
+      setTimeout(() => {
+        document.getElementById("add_university").children[1][2].click();
+        document.getElementById("add_university").children[1][4].click();
+      }, 2000);
+///mai am de lucru la asta
     });
   });
--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,5 +1,13 @@
 function add_university_form(){
     document.getElementById("add_university").children[1][12].value = 1;
+    document.getElementById("add_university").children[1][0].value = "";
+    document.getElementById("add_university").children[1][1].value = "";
+    document.getElementById("add_university").children[1][2].value = "";
+    document.getElementById("add_university").children[1][3].value = "";
+    document.getElementById("add_university").children[1][4].value = "";
+    document.getElementById("add_university").children[1][5].value = "";
+    document.getElementById("add_university").children[1][6].value = "";
+    document.getElementById("add_university").children[1][7].value = "";
     document.getElementById("edit_university").style.display = "none";
     document.getElementById("add_university").style.display = "block";
     document.getElementById("req_to_add_university").style.display = "none";
