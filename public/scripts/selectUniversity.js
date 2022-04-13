6127edf9afc74835658edbd381d2f8b349a9608e
CezarBaluta <cezar.baluta@yahoo.com>
2022-04-14 00:07:22 +0300

    editUni fix

--- public/scripts/selectUniversity.js
+++ public/scripts/selectUniversity.js
@@ -7,10 +7,10 @@ document.querySelectorAll('[universitate]').forEach(item => {
         document.getElementById("add_university").children[1][0].value = item.children[1].children[0].innerText;;
         document.getElementById("add_university").children[1][1].value = item.children[0].children[0].children[1].children[0].children[3].innerText;
         document.getElementById("add_university").children[1][2].value = item.children[1].children[1].innerText;
-        document.getElementById("add_university").children[1].children[0].children[2].children[1].children[3].innerHTML = item.children[1].children[1].innerText
+        document.getElementById("add_university").children[1].children[0].children[2].children[1].children[3].innerHTML = item.children[1].children[1].innerText;
         document.getElementById("add_university").children[1].children[0].children[2].children[1].children[3].classList.remove("default");
-        document.getElementById("add_university").children[1][4].value = item.children[1].children[2].innerText;
-        document.getElementById("add_university").children[1][5].value = item.children[1].children[3].innerText;
+        document.getElementById("add_university").children[1][4].value = item.children[1].children[3].innerText;
+        document.getElementById("add_university").children[1][5].value = item.children[1].children[2].innerText;
         document.getElementById("add_university").children[1][6].value = item.children[1].children[4].innerText;
         document.getElementById("add_university").children[1][7].value = item.children[1].children[5].innerText;
         document.getElementById("add_university").children[1][12].value = 0;
@@ -22,7 +22,6 @@ document.querySelectorAll('[universitate]').forEach(item => {
         var image = document.createElement('div');
         image.setAttribute('class', "edit_photo");
         image.innerHTML = images[0].innerHTML;
-     //   console.log(image);
         document.getElementById("add_university").children[1].appendChild(image);
         document.getElementById("add_university").children[1].insertBefore(image,document.getElementById("add_university").children[1].children[4])
       });
