9f9d22a9dd7a410a90b0386888dd7deeb2400d21
CezarBaluta <cezar.baluta@yahoo.com>
2022-04-16 23:13:34 +0300

    reqToAdd update

--- public/scripts/editReq.js
+++ public/scripts/editReq.js
@@ -1,16 +1,19 @@
 document.querySelectorAll('[req]').forEach(item => {
-    item.addEventListener('click', event => {
-        console.log("lolll");
+    var Button = item.querySelectorAll('[reqButton]')[0];
+    console.log(Button);
+    Button.addEventListener('click', event => {
+        console.log(item);
         document.getElementById("req_to_add_university").style.display = "none";
         document.getElementById("edit_university").style.display = "none";
         document.getElementById("add_university").style.display = "block";
         document.getElementById("add_university").children[1][12].value = 0;
-        document.getElementById("add_university").children[1][0].value = item.children[1].children[0].innerText;;
+        document.getElementById("add_university").children[1][0].value = item.children[1].children[0].innerText;
         document.getElementById("add_university").children[1][1].value = item.children[0].children[0].children[1].children[0].children[3].innerText;
-        document.getElementById("add_university").children[1][2].value = item.children[1].children[0].innerText;
-        document.getElementById("add_university").children[1][3].value = item.children[1].children[1].innerText;
-        document.getElementById("add_university").children[1][4].value = item.children[1].children[2].innerText;
-        document.getElementById("add_university").children[1][5].value = item.children[1].children[3].innerText;
+        document.getElementById("add_university").children[1][2].value = item.children[1].children[1].innerText;
+        document.getElementById("add_university").children[1].children[0].children[2].children[1].children[3].innerHTML = item.children[1].children[1].innerText;
+        document.getElementById("add_university").children[1].children[0].children[2].children[1].children[3].classList.remove("default");
+        document.getElementById("add_university").children[1][4].value = item.children[1].children[3].innerText;
+        document.getElementById("add_university").children[1][5].value = item.children[1].children[2].innerText;
         document.getElementById("add_university").children[1][6].value = item.children[1].children[4].innerText;
         document.getElementById("add_university").children[1][7].value = item.children[1].children[5].innerText;
         document.getElementById("add_university").children[1][12].value = 1;
