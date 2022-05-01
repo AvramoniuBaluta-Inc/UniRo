0c3ebb6f49970bb23a6d1a7287d00d74879afaf1
CezarBaluta <cezar.baluta@yahoo.com>
2022-05-01 22:36:16 +0300

    filter form on mobile update

--- public/scripts/popUp.js
+++ public/scripts/popUp.js
@@ -3,6 +3,7 @@ const closePopUpButton = document.getElementById("popUp-close-button");
 const overlay = document.getElementById("overlay");
 const popUp = document.getElementById("popUp");
 
+
 openPopUpButton.addEventListener("click", () => {
   openPopUp();
 });
@@ -11,7 +12,9 @@ closePopUpButton.addEventListener("click", () => {
 });
 
 overlay.addEventListener("click", () => {
-  closePopUp();
+  let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+  if(!isMobile)
+    closePopUp();
 });
 
 function openPopUp() {
