59b8be47cb2cf25053a1f81033ac745281152a78
calinstefan025 <clavramoniu@gmail.com>
2022-06-08 20:35:30 +0300

    stats page update

--- public/scripts/statsChart.js
+++ public/scripts/statsChart.js
@@ -1,28 +1,82 @@
 const ctx = document.getElementById("myChart").getContext("2d");
+const ctx2 = document.getElementById("chart2").getContext("2d");
+const luni = [
+  "Ianuarie",
+  "Februarie",
+  "Martie",
+  "Aprilie",
+  "Mai",
+  "Iunie",
+  "Iulie",
+  "August",
+  "Septembrie",
+  "Octombrie",
+  "Noiembrie",
+  "Decembrie",
+];
+
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: [
-      "Ianuarie",
-      "Februarie",
-      "Martie",
-      "Aprilie",
-      "Mai",
-      "Iunie",
-      "Iulie",
-      "August",
-      "Septembrie",
-      "Octombrie",
-      "Noiembrie",
-      "Decembrie",
-    ],
+    labels: luni,
     datasets: [
       {
-        label: "Utilizatori platforma",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "Vizionari Pagina Universitati",
+        data: [12, 19, 3, 5, 2, 3, 5, 5, 6, 2, 10, 25, 30],
         backgroundColor: [],
         borderColor: "#000",
         tension: 0.3,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+/////////////////////////////////////////////////////
+
+const chart2 = new Chart(ctx2, {
+  type: "bar",
+  data: {
+    labels: luni,
+    datasets: [
+      {
+        label: "Utilizatori noi pe platforma",
+        data: [12, 19, 3, 5, 2, 3, 30, 40, 35, 70, 60, 40],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+        ],
+        borderWidth: 1,
       },
     ],
   },
