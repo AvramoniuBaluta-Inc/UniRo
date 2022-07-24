83f09e64ba3a61e9a4ffac2ec7b5b528dbe651ba
calinstefan025 <clavramoniu@gmail.com>
2022-07-24 15:14:16 +0300

    delete comment button design

--- public/scripts/deleteButtonForComments.js
+++ public/scripts/deleteButtonForComments.js
@@ -1,28 +1,29 @@
-function link(string){
-    var newString="";
-    var cnt = 0;
-    for(var i=0;i<string.length;i++){
-        if(string[i]==='/'){
-            cnt++;
-        }
-        if(cnt===3)
-            break;
-        newString = newString + string[i];
+function link(string) {
+  var newString = "";
+  var cnt = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === "/") {
+      cnt++;
     }
-    return newString;
+    if (cnt === 3) break;
+    newString = newString + string[i];
+  }
+  return newString;
 }
 
-window.onload = function() {
-var isAuth = document.getElementById("isAuth").innerText;
-if(isAuth === "true"){
+window.onload = function () {
+  var isAuth = document.getElementById("isAuth").innerText;
+  if (isAuth === "true") {
     var comments = document.getElementsByClassName("comment-message-box");
-    for(var i = 0;i < comments.length; i++){
-        var deleteButton = document.createElement('a');
-        deleteButton.innerHTML = "Delete";
-        deleteButton.classList.add("deleteCommentButton");
-        deleteButton.href = link(window.location.href) +"/dashboard/deletecomment/"+ comments[i].getElementsByClassName("commentId")[0].innerText;
-        comments[i].appendChild(deleteButton);
+    for (var i = 0; i < comments.length; i++) {
+      var deleteButton = document.createElement("a");
+      deleteButton.innerHTML = "STERGE";
+      deleteButton.classList.add("deleteCommentButton");
+      deleteButton.href =
+        link(window.location.href) +
+        "/dashboard/deletecomment/" +
+        comments[i].getElementsByClassName("commentId")[0].innerText;
+      comments[i].appendChild(deleteButton);
     }
-}
-
+  }
 };
