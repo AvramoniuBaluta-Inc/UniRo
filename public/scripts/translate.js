da0e9c24c10ecd0c162cc7ccc7baba4e71e18dc9
Avramoniu Calin <clavramoniu@gmail.com>
2022-07-26 04:06:34 +0300

    translate functionality implementation

---
+++ public/scripts/translate.js
@@ -0,0 +1,22 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {
+        pagelanguage:'ro',
+        autoDisplay: false,
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        },
+        'googleTranslate'
+    );
+}
+
+
+window.onload = function deleteExtraContent(){
+    var translateDiv = document.getElementById("googleTranslate");
+    console.log(translateDiv.firstChild.firstChild.children[1].firstChild);
+    translateDiv.firstChild.firstChild.children[1].firstChild.children[1].remove();
+    translateDiv.firstChild.firstChild.children[1].firstChild.children[1].remove();
+    translateDiv.firstChild.firstChild.children[1].firstChild.children[2].remove();
+
+}
+
+
